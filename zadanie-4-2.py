7cd8305fc4c5d3fd6595ca415345c423f19045b3
Lukasz Iwanski <lukasziwanski@outlook.com>
2021-06-22 21:36:08 +0200

    notatka co do zadania

--- zadanie-4-2.py
+++ zadanie-4-2.py
@@ -10,3 +10,10 @@ else:
     print("False")
 
 
+"""
+kiedy słowo jest palindromem?
+porównanie ilości znaków żeby byla taka sama
+odwrócenie słowa
+iteracyjne porównanie kazdej litery, jeżeli inna =false
+tez mozna powiedzieć że każde które ma mniej niż 2 znaki ale to takie meh
+"""
